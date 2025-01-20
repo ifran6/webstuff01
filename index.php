a2f87db74d9e2d89ccddb6051269444aaa22e79e
ifran6 <zeelink2016@gmail.com>
2025-01-20 13:27:43 +0100

    Add files via upload
    web

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php $page_name = "Home";
+include("incls/header.php"); ?>
+
+
+
+<div class="autoHight">
+    <div class="container-fluid bg-light">
+       <div class="row">
+          
+          <div class="col-md-6">
+              <div class="p-5">
+              <h1>Enrich Koncept </h1>
+            <span><p class="taggline">Creativity, Connected</p></span>
+              </div>
+          </div>
+
+        <div class="col-md-6">
+            <img src="assets/img/banner.jpeg" alt="" class="img-responsive">
+        </div>
+       </div>
+    </div>
+</div>
+
+<?php include('incls/BestSeller.php'); ?>
+<?php include('incls/newsLetter.php'); ?>
+
+<?php include('incls/footer.php'); ?>
